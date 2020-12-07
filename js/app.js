e3232d4b24acdd2c693719e4d417ada9e5f07317
Ami Onodera <onodera.camila@gmail.com>
2020-12-07 16:51:05 +0100

    intro component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+const Intro = () => (
+    <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+        <div className="mb3 mb4-ns">
+            Lost in Tokyo is a directory of fun places to see, play in and explore, in Tokyo, Japan. {' '}
+        </div>
+        <div>
+            From museums and galleries, to Robot Restaurants and kitten cafes, Tokyo is the gift that keeps on giving. Dattebayo! {' '}
+        </div>
+    </div>
+)
 
-ReactDOM.render(<Hello name="Ami" />, document.getElementById('root'));
+
+const App = () => (
+    <div>
+        <div className="min-vh-100 ph4 flex flex-column">
+            <Intro />
+        </div>
+        <div className="flex flex-wrap contyainer">
+
+        </div>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'))

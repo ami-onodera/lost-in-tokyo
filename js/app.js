f201dea05f2aaf38729fa01df003b3b42b5f5f8c
Ami Onodera <onodera.camila@gmail.com>
2020-12-08 15:18:28 +0100

    added logo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const Highlight = props => (
-    <span className={`relative highlight highlight-${props.color}`}>
-        <span className="relative z-2">{props.children}</span>
+const Highlight = ({color, children}) => (
+    <span className={`relative highlight highlight-${color}`}>
+        <span className="relative z-2">{children}</span>
     </span>
     );
 
@@ -16,13 +16,20 @@ const Intro = () => (
     </div>
 )
 
+const NavItem = ({className, href, children, logo}) => (
+    <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+        <a className='white no-underline' href={href}>
+            {/* check if has a logo, if not, renders children */}
+            {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+        </a>
+    </li>
+)
+
 const Nav = () => (
     <nav className='pt3 pt4-ns mb4 mb0-ns'>
         <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0'>
             {menu.map(item => (
-                <li className={`mh2-ns f6 f4-1 tc ${item.className}`}>
-                    <a className='white no-underline' href={item.href}>{item.children}</a>
-                </li>
+                <NavItem {...item} />
             ))}
         </ul>
     </nav>

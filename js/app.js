2e683943b14665a2c9cbb557a124188a9e57460d
Ami Onodera <onodera.camila@gmail.com>
2020-12-19 14:47:04 +0100

    added class

--- js/app.js
+++ js/app.js
@@ -1,68 +1,86 @@
-const Highlight = ({color, children}) => (
-    <span className={`relative highlight highlight-${color}`}>
-        <span className="relative z-2">{children}</span>
-    </span>
-    );
-
-
-const Intro = () => (
-    <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
-        <div className="mb3 mb4-ns">
-            <Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color="yellow">explore</Highlight>, in <Highlight color="blue">Tokyo</Highlight>, Japan.
-        </div>
-        <div>
-            From <Highlight color="blue">museums</Highlight> and <Highlight color="blue">galleries</Highlight>, to <Highlight color="pink">Robot Restaurants</Highlight> and <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that keeps on giving. <Highlight color="yellow">Dattebayo</Highlight>! 
-        </div>
-    </div>
-)
-
-const NavItem = ({className, href, children, logo}) => (
-    <li className={`mh2-ns f6 f4-1 tc ${className}`}>
-        <a className='white no-underline' href={href}>
-            {/* check if has a logo, if not, renders children */}
-            {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
-        </a>
-    </li>
-)
-
-const Nav = () => (
-    <nav className='pt3 pt4-ns mb4 mb0-ns'>
-        <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0'>
-            {menu.map(item => (
-                <NavItem {...item} />
-            ))}
-        </ul>
-    </nav>
-)
-
-const Attraction = ({title, description, className, image}) => (
-  <div
-    className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
-  >
-    <div className="relative">
-      <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
-        <div>
-          <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
-          <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
-        </div>
-      </div>
-      <img src={`../images/${image}`} className="db" />
-    </div>
-  </div>
-)
-
-const App = () => (
-    <div>
-        <div className="min-vh-100 ph4 flex flex-column">
-            <Nav />
-            <Intro />
-        </div>
-        <div className="flex flex-wrap container">
-            {attractions.map(attraction =>
-                <Attraction {...attraction} />
-            )}
-        </div>
-    </div>
+const Highlight = ({ color, children }) => (
+  <span className={`relative highlight highlight-${color}`}>
+    <span className="relative z-2">{children}</span>
+  </span>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Intro = () => (
+  <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+    <div className="mb3 mb4-ns">
+      <Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun
+      places to see, play in and <Highlight color="yellow">explore</Highlight>,
+      in <Highlight color="blue">Tokyo</Highlight>, Japan.
+    </div>
+    <div>
+      From <Highlight color="blue">museums</Highlight> and{" "}
+      <Highlight color="blue">galleries</Highlight>, to{" "}
+      <Highlight color="pink">Robot Restaurants</Highlight> and{" "}
+      <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that
+      keeps on giving. <Highlight color="yellow">Dattebayo</Highlight>!
+    </div>
+  </div>
+);
+
+const NavItem = ({ className, href, children, logo }) => (
+  <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+    <a className="white no-underline" href={href}>
+      {/* check if has a logo, if not, renders children */}
+      {logo ? (
+        <img src="../images/logo.svg" className="db center logo" />
+      ) : (
+        children
+      )}
+    </a>
+  </li>
+);
+
+const Nav = () => (
+  <nav className="pt3 pt4-ns mb4 mb0-ns">
+    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
+      {menu.map((item) => (
+        <NavItem {...item} />
+      ))}
+    </ul>
+  </nav>
+);
+
+class Attraction extends React.Component {
+  render() {
+    const { title, description, className, image } = this.props;
+    return (
+      <div
+        className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
+      >
+        <div className="relative">
+          <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
+            <div>
+              <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">
+                {title}
+              </h1>
+              <p className="lh-title lh-copy-ns mv0 black f6 measure-l">
+                {description}
+              </p>
+            </div>
+          </div>
+          <img src={`../images/${image}`} className="db" />
+        </div>
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      <Nav />
+      <Intro />
+    </div>
+    <div className="flex flex-wrap container">
+      {attractions.map((attraction) => (
+        <Attraction {...attraction} />
+      ))}
+    </div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
